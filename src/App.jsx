b97b9a8ebd4335695f8286ac8d2618c77598732b
Mehefuj Ali <mehefujalim@gmail.com>
2024-10-23 22:08:19 +0530

    nav bar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css'
+import Nav from './components/Nav/Nav'
 
 function App() {
 
 
   return (
     <>
-
+      <header >
+        <div className=' bg-sky-500'>
+          <Nav></Nav>
+        </div>
+      </header>
     </>
   )
 }
