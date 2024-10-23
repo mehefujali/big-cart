7dbc6ac221a326ecc18a12a6a3643e3f3fb58590
Mehefuj Ali <mehefujalim@gmail.com>
2024-10-24 00:09:58 +0530

    cart btn and rating added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,11 +1,27 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
+import { IoIosStar } from "react-icons/io";
+import { IoCartOutline } from "react-icons/io5";
+import { IoCart } from "react-icons/io5";
 const Product = ({ product }) => {
-      const { title, image, price } = product
+      const { title, image, price, rating } = product
+      const [cartAddStatus, setCartAddStatus] = useState(false)
+      const handelAddCart = () => {
+            setCartAddStatus(true)
+      }
       return (
-            <div className=" flex flex-col gap-1 w-full h-full">
-                  <img className=" w-44 h-44 object-contain" src={image} alt="" />
-                  <h1 className=" font-bold">{title}</h1>
-                  <p>$ {price}</p>
+            <div className=" 
+            flex flex-col gap-1 w-full h-full border p-4  text-start rounded-lg
+            shadow-sm
+            
+            ">
+                  <img className=" w-44 h-44 object-contain mx-auto" src={image} alt="" />
+                  <div className=" mt-3 flex flex-col gap-1">
+                        <h1 className=" font-bold">{title}</h1>
+                        <p>$ {price}</p>
+                        <p className=" flex items-center gap-1">{rating.rate} <IoIosStar className=" text-yellow-600" /> <span >{rating.count} rating</span></p>
+                  </div>
+                  <button className="btn w-fit mt-4 bg-transparent border-2 border-sky-500 text-sky-500  hover:bg-transparent" onClick={handelAddCart}>{cartAddStatus ? <IoCart className="text-xl" /> : <IoCartOutline className=" text-xl" />} Add to cart</button>
             </div>
       );
 };
